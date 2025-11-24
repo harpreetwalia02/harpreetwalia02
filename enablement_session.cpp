5de457888163cccb9799b31442c6a689bc406caa
Rovo Dev <a393d78c-73d8-4f4d-84ea-500a2d3d8fc3@connect.atlassian.com>
2025-11-24 07:58:01 +0000

    CG-15

---
+++ enablement_session.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+/**
+ * Enablement session
+ * 
+ * A simple C++ program to print "Hello TechM and SSEs"
+ * Created for Jira issue CG-15
+ */
+
+int main() {
+    // Print the required message
+    std::cout << "Hello TechM and SSEs" << std::endl;
+    
+    return 0;
+}
